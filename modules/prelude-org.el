c869ee9ed49350b8b73918398c9d208b5f521a0a
EthanFu <fuxinjie0309@gmail.com>
2016-06-16 18:50:11 +0800

    modify fu's config of orgmode

--- modules/prelude-org.el
+++ modules/prelude-org.el
@@ -36,6 +36,7 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cc" 'org-capture)
 (setq org-log-done t)
 
 (defun prelude-org-mode-defaults ()
@@ -55,3 +56,101 @@
 (provide 'prelude-org)
 
 ;;; prelude-org.el ends here
+
+;; -----------------------------------------------------------------------------
+;; ethanfu's config
+;; record the closed note
+(setq org-log-done 'note)
+;; -----------------------------------------------------------------------------
+;;set up capture
+(setq org-directory "~/gitfu/fuprivate/notes/")
+(setq org-default-notes-file (concat org-directory "fnote.org"))
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/Dropbox/gtd/gtdfu.org" "Tasks")
+	 "* TODO %?\n  %i\n %a" :clock-in t :clock-resume t :prepend t)
+	("i" "Inbox" entry (file+headline (concat org-directory "inbox.org") "Inbox")
+	 "* %?\n %i\n %T\n %a")
+	("n" "Note" entry (file+headline (concat org-directory "fnote.org") "Note")
+	 "* %?\n %i\n %T\n %a")
+	("v" "Vocabulary" entry (file+headline (concat org-directory "vocabulary.org") "Vocabulary")
+         "* %^{The word} \n:PROPERTIES:\n:Part-of-speech: %^{Part of speech|verb|noun|adj|adv}\n \n %t\n %^{Extended word (may be empty)} \n** Answer \n%^{The definition}")
+        ("j" "Journal" entry (file+datetree (concat org-directory "journal.org"))
+	 "* %?\nEntered on %U\n  %i\n  %a")))
+;; ----------------------------------------------------------------------------
+;; load markdown expordown exporter automatically with org-mode
+(eval-after-load "org"
+  '(require 'ox-md nil t))
+;; -----------------------------------------------------------------------------
+;; orgmode export to pdf,using xelatex
+;; add to org header, add the ctex package, and set the page size
+;; #+LATEX_HEADER: \usepackage{ctex}
+(setq org-latex-packages-alist
+      '("
+\\usepackage{ctex}
+\\usepackage{graphicx}
+\\usepackage[top=2.54cm, bottom=2.54cm, left=3.17cm, right=3.17cm]{geometry}
+"))
+(setq org-latex-compiler "XeLaTex")
+(setq org-latex-pdf-process '("xelatex -interaction nonstopmode %f"
+                              "xelatex -interaction nonstopmode %f"))
+;; -----------------------------------------------------------------------------
+;; babel
+(org-babel-do-load-languages
+ (quote org-babel-load-languages)
+ (quote ((emacs-lisp . t)
+         (java . t)
+         (dot . t)
+         (ditaa . t)
+         (R . t)
+         (python . t)
+         (ruby . t)
+         (gnuplot . t)
+         (clojure . t)
+         (sh . t)
+         (ledger . t)
+         (org . t)
+         (plantuml . t)
+         (latex . t)
+	 (C .t)
+	 )))
+;; bebel evaluate don't ask questions
+(setq org-confirm-babel-evaluate nil)
+;; -----------------------------------------------------------------------------
+;; display babel pictures
+;; C-c C-c then C-c C-x C-v
+(add-hook 'org-babel-after-execute-hook 'bh/display-inline-images 'append)
+;; Make babel results blocks lowercase
+(setq org-babel-results-keyword "results")
+
+(defun bh/display-inline-images ()
+  (condition-case nil
+      (org-display-inline-images)
+    (error nil)))
+;; -----------------------------------------------------------------------------
+;; set plantuml path
+(setq org-plantuml-jar-path
+      (expand-file-name "/usr/local/Cellar/emacs/24.5/share/emacs/24.5/lisp/contrib/scripts/plantuml.jar"))
+
+;; -----------------------------------------------------------------------------
+;; plantumlDemo
+;; #+begin_src plantuml :file tryout.png :cmdline -charset UTF-8
+;;  Alice -> Bob: synchronous call
+;;  Alice ->> Bob: asynchronous call
+;; #+end_src
+;; dita
+;; #+BEGIN_SRC ditaa :file ${1:export-file-name} :cmdline -r -s 0.8
+;; ${0}
+;; #+END_SRC
+;; dot
+;; #+BEGIN_SRC dot :file ${1:export-file-name}.png :cmdline -Kdot -Tpng
+;; title ${0}
+;; #+END_SRC
+;; uml
+;; #+BEGIN_SRC plantuml :file ${1:export-file-name} :cmdline -charset UTF-8
+;; title ${0}
+;; #+END_SRC
+;; -----------------------------------------------------------------------------
+;; org默认使用"_下标"来定义一个下标，使用"^上标"定义一个上标，但这种方式在中文环境中与下划线冲突。
+;; 这里强制使用"_{下标}"来定义一个下标。"^{上标}"来定义一个上标。
+;; (setq org-export-with-sub-superscripts '{})
+;; (setq org-use-sub-superscripts '{})
