02ec068005e58363e26b48b5437578ef6bc9ceda
EthanFu <fuxinjie0309@gmail.com>
2017-07-21 17:43:23 +0800

    add customize export html css

--- modules/prelude-org.el
+++ modules/prelude-org.el
@@ -155,3 +155,44 @@
 ;; 这里强制使用"_{下标}"来定义一个下标。"^{上标}"来定义一个上标。
 ;; (setq org-export-with-sub-superscripts '{})
 ;; (setq org-use-sub-superscripts '{})
+
+;; 设置org 导出为html的css样式；css统一放在.emacs.d/org-css
+;; put your css files there
+(defvar org-theme-css-dir "~/.emacs.d/org-css/")
+
+(defun toggle-org-custom-inline-style ()
+  (interactive)
+  (let ((hook 'org-export-before-parsing-hook)
+        (fun 'set-org-html-style))
+    (if (memq fun (eval hook))
+        (progn
+          (remove-hook hook fun 'buffer-local)
+          (message "Removed %s from %s" (symbol-name fun) (symbol-name hook)))
+      (add-hook hook fun nil 'buffer-local)
+      (message "Added %s to %s" (symbol-name fun) (symbol-name hook)))))
+
+(defun org-theme ()
+  (interactive)
+  (let* ((cssdir org-theme-css-dir)
+         (css-choices (directory-files cssdir nil ".css$"))
+         (css (completing-read "theme: " css-choices nil t)))
+    (concat cssdir css)))
+
+(defun set-org-html-style (&optional backend)
+  (interactive)
+  (when (or (null backend) (eq backend 'html))
+    (let ((f (or (and (boundp 'org-theme-css) org-theme-css) (org-theme))))
+      (if (file-exists-p f)
+          (progn
+            (set (make-local-variable 'org-theme-css) f)
+            (set (make-local-variable 'org-html-head)
+                 (with-temp-buffer
+                   (insert "<style type=\"text/css\">\n<!--/*--><![CDATA[/*><!--*/\n")
+                   (insert-file-contents f)
+                   (goto-char (point-max))
+                   (insert "\n/*]]>*/-->\n</style>\n")
+                   (buffer-string)))
+            (set (make-local-variable 'org-html-head-include-default-style)
+                 nil)
+            (message "Set custom style from %s" f))
+        (message "Custom header file %s doesnt exist")))))
