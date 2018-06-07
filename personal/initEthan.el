9bb3f1cd8836190e527bb9a4f6646343bf3d9f7d
EthanFu <fuxinjie0309@gmail.com>
2018-06-07 08:24:32 +0800

    modify artist mode key map

--- personal/initEthan.el
+++ personal/initEthan.el
@@ -8,3 +8,14 @@
 ;; add the tex path in emacs
 (setenv "PATH" "/usr/local/bin:/Library/TeX/texbin/:$PATH" t)
 (setq exec-path (append exec-path '("/Library/TeX/texbin")))
+
+;; artist mode key map
+
+(add-hook 'artist-mode-hook
+          (lambda()
+            (local-set-key (kbd "s-1") 'org-mode)
+            (local-set-key (kbd "s-2") 'artist-select-op-pen-line) ;;f2=pen mode
+            (local-set-key (kbd "s-3") 'artist-select-op-line) ;;f3=line
+            (local-set-key (kbd "s-4") 'artist-select-op-square) ;;f4=rectangle
+            (local-set-key (kbd "s-5") 'artist-select-op-ellipse)  ;;f5=ellipse
+           ))
